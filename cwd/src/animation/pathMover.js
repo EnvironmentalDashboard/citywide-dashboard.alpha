739d62b56da49506f5a24bb7523087ae8840ff7d
Colton Potter <cpotter2@oberlin.edu>
2019-07-15 15:27:42 -0400

    Modify pathMover for <svg> tags

--- cwd/src/animation/pathMover.js
+++ cwd/src/animation/pathMover.js
@@ -30,16 +30,23 @@ export const pathMover = state => {
       let lengthOnPath =
         (current / duration) * state.path.$link.getTotalLength();
 
+      let linkTransformable = state.$link.tagName.toUpperCase() === 'SVG' ? false : true;
+
       /**
        * @todo This may be leakly logic...
        */
       let point = state.path.$link.getPointAtLength(lengthOnPath);
-      const transform = getCompositeTransform(
-        state,
-        `translate(${point.x}px, ${point.y}px)`
-      );
 
-      state.style = Object.assign(state.style, { transform });
+      if (linkTransformable) {
+        const transform = getCompositeTransform(
+          state,
+          `translate(${point.x}px, ${point.y}px)`
+        );
+        state.style = Object.assign(state.style, { transform });
+      } else {
+        state.style = Object.assign(state.style, { x: point.x, y: point.y });
+      }
+
     },
 
     readyToStep: function(timestamp) {
