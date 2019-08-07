c20edf720ff9fa34812e4d2eaf560170d3ca2863
Colton Potter <cpotter2@oberlin.edu>
2019-08-07 11:57:53 -0400

    fix(src/index): Remove svgGradient

--- cwd/src/index.js
+++ cwd/src/index.js
@@ -3,7 +3,6 @@ export { glyph } from './glyph';
 export { graphic } from './graphic';
 export { pathShape } from './graphic/pathShape';
 export { svgImageShape } from './graphic/svgImageShape';
-export { svgGradient } from './graphic/svgGradient';
 export { svgShape } from './graphic/svgShape';
 
 export { events } from './events';
