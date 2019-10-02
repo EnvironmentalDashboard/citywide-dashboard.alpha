6324eec2f9c4a969b038dc97f39f470fff8c75f5
Sam Craig <craigsamm@gmail.com>
2019-10-02 16:33:36 -0400

    Add documentation for switching the path

--- cwd/run.sh
+++ cwd/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# add this as an option to switch to using a different path
+# -e "PATH_PREFIX=citywide-dashboard"
 docker run -dit -p 42000:80 -v $(pwd):/usr/src/app --restart always --name cwd citywide-dashboard
