6ec38ef79012f7ed240c0df212915df694b85f67
acrap5 <doublebass49@gmail.com>
2019-11-20 16:24:17 -0500

    Some more syntax changes/clean ups to address comments made on the pull request.

--- cwd/app.js
+++ cwd/app.js
@@ -2,8 +2,7 @@
   let svg = document.getElementById('svg-wrap');
 
   const allGlyphs = activeGlyphs.arr;
-  views = allGlyphs.filter(obj => obj.view).map(obj => obj.view);
-  views = views.map(v => ({ view: v, hash: v.name}))
+  views = allGlyphs.filter(obj => obj.view).map(obj => obj.view).map(v => ({ view: v, hash: v.name}));
 
   // This makes sure width isn't too big for the screen, and switches to calculate based off of full width
   var height =
@@ -46,7 +45,7 @@
         }
 
         if(SHOW_ONE_TITLE) {
-          let buttons = Array.prototype.slice.call(document.getElementsByClassName("glow-on-hover")).filter(obj => obj.id.includes("Button"));
+          const buttons = Array.prototype.slice.call(document.getElementsByClassName("glow-on-hover")).filter(obj => obj.id.includes("Button"));
           const index = buttons.indexOf(document.getElementById(this.id));
           buttons[index].style.display = "none";
 
@@ -226,7 +225,7 @@
     if (SHOW_ONE_TITLE) {
       for (let i = 0; i < views.length; i++) {
         var elt = document.getElementById(`${views[i].hash}Button`);
-        if (i != views.findIndex(i => i.hash === window.location.hash)) {
+        if (i !== views.findIndex(i => i.hash === window.location.hash)) {
           elt.style.display = "none";
         }
         elt.setAttribute("x", "83%");
@@ -316,14 +315,13 @@
    */
 
   function startKiosk(duration) {
-
     let index = 0;
 
     if (window.location.hash) {
 
       hash = window.location.hash.substr(1,);
 
-      if (!(views.filter(v => v.hash === hash).length > 0)) {
+      if (!views.find(v => v.hash === hash)) {
         console.error('Invalid hash: ' + window.location.hash);
         window.location.hash = '';
       } else {
