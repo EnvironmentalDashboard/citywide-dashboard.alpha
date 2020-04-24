495b8d9237a64968ffdd45c88b10bf9339f01980
acrap5 <acrapitt@oberlin.edu>
2020-04-24 16:51:12 +0000

    More fixes to adjust for using relative routes instead of hardcoded ones

--- cwd/editor.js
+++ cwd/editor.js
@@ -1,15 +1,15 @@
-const update = (btn) => {
+const update = (btn, view) => {
   const viewNum = btn.id.substring(8, 9);
   const gaugeNum = btn.id.substring(10, 11);
   const msgNum = btn.id.substring(12, );
 
   const prob = [];
-  for (let x = 0; x <= 15; x++) {
+  for (let x = 0; x < 5; x++) {
     prob.push($('#prob-' + (btn.id.substring(8, )) + "-" + (x + 1)).val());
   }
 
   const request = {
-    "url": `${views[viewNum - 1]}gauges/${gaugeNum}/messages/${msgNum}`,
+    "url": `http://${API_URL}/glyphs/${view}/gauges/${gaugeNum}/messages/${msgNum}`,
     "type": "POST",
     "timeout": 0,
     "headers": {
@@ -30,7 +30,59 @@ const update = (btn) => {
   });
 }
 
-const updateView = (btn) => {
+// Note: this function is subject to change as I change how the backend handles this
+const add = (btn, view) => {
+  const viewNum = btn.id.substring(8, 9);
+  const gaugeNum = btn.id.substring(10, 11);
+  const msgNum = btn.id.substring(12, );
+
+  const prob = [];
+  for (let x = 0; x < 5; x++) {
+    prob.push($('#prob-' + (btn.id.substring(8, )) + "-" + (x + 1)).val());
+  }
+
+  let request = {
+    "url": `http://${API_URL}/glyphs/${view}/gauges/${gaugeNum}/messages/`,
+    "type": "POST",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    "data": {
+      "pass": "\"" + $('#passIn').val() + "\""
+    }
+  };
+
+  $.ajax(request).done(function(response) {
+    if (response.errors){
+      alert(response.errors + " Please try again.");
+    }
+    console.log(response);
+  });
+
+  request = {
+    "url": `http://${API_URL}/glyphs/${view}/gauges/${gaugeNum}/messages/0`,
+    "type": "POST",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    "data": {
+      "message": "\"" + $('#input-' + (btn.id.substring(8, ))).val() + "\"",
+      "probability": `\[${prob}\]`,
+      "pass": "\"" + $('#passIn').val() + "\""
+    }
+  };
+
+  $.ajax(request).done(function(response) {
+    if (response.errors){
+      alert(response.errors + " Please try again.");
+    }
+    console.log(response);
+  });
+}
+
+const updateView = (btn, view) => {
   const viewNum = btn.id.substring(8, 9);
   const msgNum = btn.id.substring(10, 11);
 
@@ -38,7 +90,7 @@ const updateView = (btn) => {
   prob.push($('#prob-' + (btn.id.substring(8, )) + "-" + (1)).val());
 
   const request = {
-    "url": `${views[viewNum - 1]}gauges/0/messages/${msgNum}`,
+    "url": `http://${API_URL}/glyphs/${view}/messages/${msgNum}`,
     "type": "POST",
     "timeout": 0,
     "headers": {
@@ -63,6 +115,7 @@ const makeButton = (id, div, text, ref) => {
   let upBtn = document.createElement("button");
   upBtn.setAttribute("type", "button");
   upBtn.setAttribute("id", id)
+  upBtn.setAttribute("parent", div.id)
   upBtn.innerHTML = text
 
   if (ref) {
@@ -121,7 +174,7 @@ $.getJSON(`http://${API_URL}/views`, function(views) {views.forEach(view => {
     let viewMsgItr = 0;
     viewItr++;
 
-    data.view.messages.forEach(element => {
+    data.view.messages.forEach((element, index) => {
       viewMsgItr++;
 
       const currentDiv = document.getElementById(data.name);
@@ -133,10 +186,10 @@ $.getJSON(`http://${API_URL}/views`, function(views) {views.forEach(view => {
       makeButton('post-btn' + viewItr + "-" + viewMsgItr, currentDiv, "Update", null);
 
       $('#post-btn' + viewItr + "-" + viewMsgItr).click(function() {
-        updateView(this);
+        updateView(this, view._id);
       });
 
-      if (data.view.messages.indexOf(element) === data.view.messages.length - 1) {
+      if (index === data.view.messages.length - 1) {
         currentDiv.appendChild(document.createElement("BR"));
         makeButton('add-btn' + viewItr, currentDiv, "Add to View", null);
         viewMsgItr++;
@@ -149,7 +202,7 @@ $.getJSON(`http://${API_URL}/views`, function(views) {views.forEach(view => {
           makeButton("post-btn" + id + viewMsgItr, currentDiv, "Update", this);
 
           document.getElementById("post-btn" + id + viewMsgItr).addEventListener("click", function() {
-            updateView(this);
+            updateView(this, view._id);
           });
 
           currentDiv.insertBefore(document.createElement("BR"), this);
@@ -157,14 +210,40 @@ $.getJSON(`http://${API_URL}/views`, function(views) {views.forEach(view => {
       }
       currentDiv.appendChild(document.createElement("BR"));
     });
-    data.view.gauges.forEach(gauge => {
-      let gaugeItr = 0;
+
+    let gaugeItr = 0;
+    data.view.gauges.forEach((gauge, index) => {
       gaugeItr++;
       let messageItr = 0;
 
       currentDiv = document.getElementById(data.name);
 
-      if (typeof(gauge.messages) == 'undefined') {
+      if (typeof(gauge.messages) === 'undefined') {
+        makeButton('add-btn' + viewItr + '-' + gaugeItr, currentDiv, `Add to Gauge ${gaugeItr}`, null);
+        currentDiv.append(document.createElement("BR"));
+
+        $('#add-btn' + viewItr + '-' + gaugeItr).click(function() {
+          currentDiv = document.getElementById(this.getAttribute("parent"));
+
+          messageItr++;
+          const id = (this.id.substring(7, 8)) + "-" + (this.id.substring(9, )) + "-" + messageItr;
+
+          makeTextInput("input-" + id, currentDiv, "", this);
+
+          for (const x of Array(5).keys()) {
+            makeNumInput("prob-" + id + "-" + (x + 1), currentDiv, 0, this);
+          }
+
+          makeButton("post-btn" + id, currentDiv, "Update", this);
+
+          document.getElementById("post-btn" + id).addEventListener("click", function() {
+            add(this, view._id);
+          });
+
+          currentDiv.insertBefore(document.createElement("BR"), this);
+        });
+
+
         return
       }
 
@@ -185,7 +264,7 @@ $.getJSON(`http://${API_URL}/views`, function(views) {views.forEach(view => {
         makeButton('post-btn' + viewItr + "-" + gaugeItr + "-" + messageItr, currentDiv, "Update", null);
 
         $('#post-btn' + viewItr + "-" + gaugeItr + "-" + messageItr).click(function() {
-          update(this);
+          update(this, view._id);
         });
 
       })
@@ -205,7 +284,7 @@ $.getJSON(`http://${API_URL}/views`, function(views) {views.forEach(view => {
         makeButton("post-btn" + id, currentDiv, "Update", this);
 
         document.getElementById("post-btn" + id).addEventListener("click", function() {
-          update(this);
+          update(this, view._id);
         });
 
         currentDiv.insertBefore(document.createElement("BR"), this);
