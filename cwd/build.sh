9e7dd3bb2afa2188dc96ea300ba34db418467658
Sam Craig <craigsamm@gmail.com>
2020-12-19 17:39:46 -0500

    Run in production on our production servers

--- cwd/build.sh
+++ cwd/build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker build -t citywide-dashboard .
+production_domain=communityhub.cloud
+domain=`cut -f 2- -d . <<< $HOSTNAME`
+
+if [ "$domain" = "$production_domain" ] || [ "$HOSTNAME" = "$production_domain" ]
+then
+  env=prod
+else
+  env=dev
+fi
+
+docker build  --build-arg APP_ENV=$env -t citywide-dashboard .
